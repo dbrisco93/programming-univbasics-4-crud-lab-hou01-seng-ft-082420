4bfb1bf655ee04ff7a20fe5af601419208e5c559
Dominique Brisco <dbrisco93@gmail.com>
2020-08-08 05:39:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  create_an_array = ["wow", "I", "am", "learning"]
+  add_element_to_end_of_array = ["wow", "I", "am", "learning"]
   create_an_array << "arrays!"
 end
 
