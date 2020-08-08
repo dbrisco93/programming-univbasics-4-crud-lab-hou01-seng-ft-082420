ade2251fc25b70dd24090d407a0d44ea83bd62a0
Dominique Brisco <dbrisco93@gmail.com>
2020-08-08 05:34:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  create_an_array = ["blue", "gold", "red", "white"]
+  create_an_array.push("green")
 end
 
 def add_element_to_start_of_array(array, element)
